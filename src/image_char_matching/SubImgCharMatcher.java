ea7a4b05c387ba56e64673a92843fcd04822a889
tamir99 <tamir.perry@mail.huji.ac.il>
2024-02-29 17:15:40 +0200

    changed the inner data structure in SubImgCharMatcher. added documentation.

--- src/image_char_matching/SubImgCharMatcher.java
+++ src/image_char_matching/SubImgCharMatcher.java
@@ -1,16 +1,23 @@
 package image_char_matching;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
+/**
+ * The SubImgCharMatcher class is responsible to hold a set of characters and match them a brightness value
+ * between 0 and 1.
+ */
 public class SubImgCharMatcher {
     //fields
-    private final TreeMap<Double, Set<Character>> charsBrightness;
+    private final TreeMap<Double, PriorityQueue<Character>> charsBrightness;
     private double max_brightness, min_brightness;
 
+    /**
+     * Constructs a SubImgCharMatcher object with the given character set.
+     *
+     * @param charset The character set to hold.
+     */
     public SubImgCharMatcher(char[] charset) {
+        //setting values that will change in the first iterations
         max_brightness = 0;
         min_brightness = 1;
         charsBrightness = new TreeMap<>();
@@ -19,13 +26,26 @@ public class SubImgCharMatcher {
         }
     }
 
+    /**
+     * Finds the char with the closest brightness to a given value.
+     * @param brightness value between 0 and 1
+     * @return a char in the set that best represent the brightness value.
+     */
     public char getCharByImageBrightness(double brightness) {
+        // corrects the brightness to the range of brightnesses of the chars
         double correctedBrightness = correctBrightness(brightness);
         Double closestKey = findClosestKey(correctedBrightness);
-        Set<Character> set = charsBrightness.get(closestKey);
-        return Collections.min(set);
+        //finds the best char with this brightness
+        PriorityQueue<Character> minHeap = charsBrightness.get(closestKey);
+        //can throw NullPointerException but we assume it's never empty
+        return minHeap.peek();
     }
 
+    /**
+     * finds the brightness value closest to a given value
+     * @param brightness Value between 0 and 1
+     * @return A char with the brightness closest to brightness
+     */
     private Double findClosestKey(double brightness) {
         Double floorKey = charsBrightness.floorKey(brightness);
         Double ceilingKey = charsBrightness.ceilingKey(brightness);
@@ -40,7 +60,7 @@ public class SubImgCharMatcher {
             else {
                 double floorDiff = Math.abs(brightness - floorKey);
                 double ceilingDiff = Math.abs(brightness - ceilingKey);
-
+                //selects the value with the smallest absolute distance.
                 if (floorDiff <= ceilingDiff) {
                     return floorKey;
                 } else {
@@ -50,37 +70,53 @@ public class SubImgCharMatcher {
         }
     }
 
+    /**
+     * Apllies a linear transformation to brightness, moving it from the range [0,1] to
+     * [min_brightness, max_brightness]
+     * @param brightness value in range [0,1]
+     * @return value in range [min_brightness, max_brightness]
+     */
     private double correctBrightness(double brightness) {
         return brightness * (max_brightness - min_brightness) + min_brightness;
     }
 
-
+    /**
+     * Adds a char to the database
+     * @param c char to add
+     */
     public void addChar(char c) {
         double charBrightness = determineBrightnessValue(c);
+        //checks for new max or min
         if(charBrightness > max_brightness) {
             max_brightness = charBrightness;
         }
         if (charBrightness < min_brightness) {
             min_brightness = charBrightness;
         }
+        //checks if this brightness value already exist
         if (charsBrightness.containsKey(charBrightness)) {
             charsBrightness.get(charBrightness).add(c);
         }
         else {
-            // If the key doesn't exist, create a new set and add the character to it
-            Set<Character> charSet = new HashSet<>();
-            charSet.add(c);
-            charsBrightness.put(charBrightness, charSet);
+            // If the key doesn't exist, create a new priority queue and adds the character to it
+            PriorityQueue<Character> characterPriorityQueue = new PriorityQueue<>();
+            characterPriorityQueue.add(c);
+            charsBrightness.put(charBrightness, characterPriorityQueue);
         }
     }
 
+    /**
+     * removes a character from the database
+     * @param c char to remove
+     */
     public void removeChar(char c) {
         double charBrightness = determineBrightnessValue(c);
         if (charsBrightness.containsKey(charBrightness)) {
-            Set<Character> charSet = charsBrightness.get(charBrightness);
-            charSet.remove(c);
-            if (charSet.isEmpty()) {
+            PriorityQueue<Character> charPriorityQueue = charsBrightness.get(charBrightness);
+            charPriorityQueue.remove(c);
+            if (charPriorityQueue.isEmpty()) {
                 charsBrightness.remove(charBrightness);
+                //checks if min or max need to be updated
                 if(charBrightness == min_brightness) {
                     min_brightness = charsBrightness.firstKey();
                 }
@@ -91,10 +127,17 @@ public class SubImgCharMatcher {
         }
     }
 
+    /**
+     * determine a brightness value of a given char between 0 and 1. the brightness is the ratio between
+     * the white pixels and all the cells of a char image in font 'courier new'.
+     * @param c char to determine brightness
+     * @return value between 0 and 1
+     */
     private double determineBrightnessValue(char c) {
+        //gets the char pixels in courier font
         boolean[][] boolRepresentation = CharConverter.convertToBoolArray(c);
         int arrSize = boolRepresentation.length * boolRepresentation[0].length;
-        double count = 0.0;
+        int count = 0;
         for (boolean[] booleanLine : boolRepresentation) {
             for (boolean booleanCell : booleanLine) {
                 if (booleanCell) {
@@ -102,6 +145,6 @@ public class SubImgCharMatcher {
                 }
             }
         }
-        return count / arrSize;
+        return count /(double) arrSize;
     }
 }
